c57b13098918fd1b3a27214ef04e13f629d82a7f
Nenad Pejcic <nenad.pejcicc@gmail.com>
2019-05-20 17:42:42 +0200

    Updates

---
+++ precache-manifest.59e2987662e8ed1b1acba9a641e76b08.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6fc5c2f805036c475b57",
+    "url": "/bit-book/static/js/runtime~main.94184f26.js"
+  },
+  {
+    "revision": "2da423af40a951ccdaaa",
+    "url": "/bit-book/static/js/main.659c3328.chunk.js"
+  },
+  {
+    "revision": "c8b00bc929f3053353d1",
+    "url": "/bit-book/static/js/2.15acf322.chunk.js"
+  },
+  {
+    "revision": "2da423af40a951ccdaaa",
+    "url": "/bit-book/static/css/main.4e4bbf94.chunk.css"
+  },
+  {
+    "revision": "7a421ffc83f34295e53a5b7bfa612287",
+    "url": "/bit-book/index.html"
+  }
+];
